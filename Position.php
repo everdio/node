44ac7388f39a660c9345b65ad612c38fa84f70b4
evertdf <edefeij@live.nl>
2021-01-04 16:35:23 +0100

    added limit option to findall and added new interface method count and added limit mapper support

--- Position.php
+++ Position.php
@@ -2,8 +2,8 @@
 namespace Modules\Node {
     use \Components\Validator;
     final class Position extends \Components\Validation {
-        public function __construct(int $limit) {
-            parent::__construct(sprintf("position() <= %b", $limit), [new Validator\IsString]);
+        public function __construct(string $path, int $position, int $limit) {
+            parent::__construct(sprintf("%s[position() >= %s and position() <= %s]", $path, $position, $position + $limit), [new Validator\IsString]);
         }
     }
 }
